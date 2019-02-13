e23debb9d0af77f72746108a38281b72edf865f0
Julien Guerinet <julien@guerinet.com>
2019-02-13 16:19:26 -0500

    Add descriptions

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// Programmatically calculate Pixel's age
+const now = moment(new Date());
+const end = moment("2018-06-11");
+const duration = moment.duration(now.diff(end));
+document.getElementById("pixel-age").innerText = parseInt(duration.asMonths());
