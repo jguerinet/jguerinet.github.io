786f789f3449c3a21a4645f9d65f6ab9a74442cc
Julien Guerinet <julien@guerinet.com>
2020-02-28 15:48:24 -0500

    Remove TOP, switch Pixel's age to years, update title

--- js/index.js
+++ js/index.js
@@ -2,4 +2,4 @@
 const now = moment(new Date());
 const end = moment("2018-06-11");
 const duration = moment.duration(now.diff(end));
-document.getElementById("pixel-age").innerText = parseInt(duration.asMonths());
+document.getElementById("pixel-age").innerText = parseInt(duration.asYears());
